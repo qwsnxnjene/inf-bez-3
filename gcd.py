86280583242d0fa52af98f80e49edc438c526ef1
Danil Khalimov <khalimov480@gmail.com>
2025-03-22 09:40:29 +0300

    22.03.25

--- gcd.py
+++ gcd.py
@@ -1,15 +1,31 @@
-def gcd(a, b):
-    a, b = max(a, b), min(a, b)
-    if b == 0:
-        return b
+import sys
+import time
 
+
+def gcd(a, b):
+    start = time.time()
+
+    time.sleep(1)
+
+    a, b = max(a, b), min(a, b)
+    k = 0
+    if b == 0:
+        return b, k, (time.time() - start) - 1
+
+    k += 1
     c = a % b
     while c != 0:
         a, b = b, c
         c = a % b
-    return b
+        k += 1
+    return b, k, (time.time() - start) - 1
 
 
+# sys.set_int_max_str_digits(5000)
+# A, B = int('9' * 5000), int('8' * 5000)
+# print(A)
+# print(B)
+# print(gcd(A, B))
 print(gcd(210, 45))
 print(gcd(77, 7))
 print(gcd(100, 0))
